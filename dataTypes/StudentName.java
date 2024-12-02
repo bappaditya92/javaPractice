93be1c3afddf6c3835efd1892fa0a2650bd2c0c1
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2024-12-02 18:00:34 +0530

    adding some java code

---
+++ dataTypes/StudentName.java
@@ -0,0 +1,9 @@
+package dataTypes;
+
+public class markSheet {
+    public static void main(String[] args) {
+        String name = "Bappaditya Sinha";
+
+        System.out.println(name);
+    }
+}
